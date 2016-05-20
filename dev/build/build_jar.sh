e841a74fe7ad89ee056844f63b404d8ec347d1d4
Karen Ananiev <karen.ananiev@gmail.com>
2016-05-20 20:37:49 +0300

    removed java libs

--- dev/build/build_jar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Copyright (c) 2012-2014, CKSource - Frederico Knabben. All rights reserved.
-# For licensing, see LICENSE.md
-
-# Creates CKBuilder jar file (in the "bin" directory).
-
-ant jar
-ant clean
